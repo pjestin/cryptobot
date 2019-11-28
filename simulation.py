642fb12889bbed36836542e7eeccdae64156bf2f
Pierre JESTIN <pierre.jestin@gmail.com>
2019-11-28 08:56:02 +0000

    Update EMA strategy

--- simulation.py
+++ simulation.py
@@ -16,9 +16,9 @@ from strategy.indicators import Indicators
 # from strategy.macd_ema_rsi import MacdEmaRsiStrategy
 # from strategy.resistance import ResistanceStrategy
 # from strategy.macd_ema import MacdEmaStrategy
-# from strategy.ema import EmaStrategy
+from strategy.ema import EmaStrategy
 # from strategy.macd_ema_ratio import MacEmaRatioStrategy
-from strategy.macd import MacdStrategy
+# from strategy.macd import MacdStrategy
 # from strategy.ema_rsi import EmaRsiStrategy
 # from strategy.bull_bear_macd_ema import BullBearMacdEmaStrategy
 # from strategy.rsi import RsiStrategy
@@ -39,16 +39,17 @@ def run_simulation(klines, n_ref, commission):
         klines_ref = klines[k-n_ref:k]
         price = klines_ref[-1].close_price
         current_time = klines_ref[-1].close_time
-        logging.debug('Run {}; time: {}; money: {}; price: {}'.format(k - n_ref, current_time, money[-1], price))
+        logging.debug('Run {}; time: {}; money: {}; price: {}'.format(
+            k - n_ref, current_time, money[-1], price))
 
         # action = MacdRsiStrategy.decide_action_from_data(klines_ref)
         # action = MacdMfiStrategy.decide_action_from_data(klines_ref)
         # action = ResistanceStrategy.decide_action_from_data(klines_ref, previous_price, acquired)
-        action = MacdStrategy.decide_action_from_data(klines_ref)
+        # action = MacdStrategy.decide_action_from_data(klines_ref)
         # action = MacdEmaStrategy.decide_action_from_data(klines_ref)
         # action = MacdEmaRsiStrategy.decide_action_from_data(klines_ref)
         # action = MacEmaRatioStrategy.decide_action_from_data(klines_ref, previous_price, acquired)
-        # action = EmaStrategy.decide_action_from_data(klines_ref)
+        action = EmaStrategy.decide_action_from_data(klines_ref)
         # action = EmaRsiStrategy.decide_action_from_data(klines_ref)
         # action = BullBearMacdEmaStrategy.decide_action_from_data(klines_ref)
         # action = RsiStrategy.decide_action_from_data(klines_ref)
@@ -62,24 +63,30 @@ def run_simulation(klines, n_ref, commission):
         elif acquired and action.is_sell():
             money.append((money[-1] - 1) + (1 - commission) * acquired * price)
             acquired = None
-            logging.info('Selling at {}; money: {}; time: {}'.format(price, money[-1], current_time))
+            logging.info('Selling at {}; money: {}; time: {}'.format(
+                price, money[-1], current_time))
             previous_price = price
             previous_transaction_time = current_time
             sell_times.append(current_time)
 
     if acquired:
         last_price = klines[-1].close_price
-        money.append((money[-1] - 1) + (1 - commission) * acquired * last_price)
+        money.append((money[-1] - 1) + (1 - commission)
+                     * acquired * last_price)
         acquired = None
         logging.info('Selling at {}; money: {}'.format(last_price, money[-1]))
         sell_times.append(current_time)
-    
-    market = (klines[-1].close_price - klines[0].close_price) / klines[0].close_price
-    duration = timedelta(milliseconds=(klines[-1].close_time - klines[0].close_time))
+
+    market = (klines[-1].close_price -
+              klines[0].close_price) / klines[0].close_price
+    duration = timedelta(milliseconds=(
+        klines[-1].close_time - klines[0].close_time))
     avg_per_month = money[-1] * timedelta(days=30) / duration
     avg_per_year_multiplier = (avg_per_month + 1) ** 12
-    logging.info('Money: {}; Number of transactions: {}; Market: {}'.format(money[-1], 2 * len(money), market))
-    logging.info('Duration: {}; average money per month: {}; per year: {}'.format(duration, avg_per_month, avg_per_year_multiplier))
+    logging.info('Money: {}; Number of transactions: {}; Market: {}'.format(
+        money[-1], 2 * len(money), market))
+    logging.info('Duration: {}; average money per month: {}; per year: {}'.format(
+        duration, avg_per_month, avg_per_year_multiplier))
     logging.info('Time: {}'.format(time.time() - start_time))
 
     plt.subplot(2, 1, 1)
@@ -106,10 +113,14 @@ def graph(klines):
     n_ref = 120
     klines_ref = klines[-n_ref:]
     prices = [kline.close_price for kline in klines_ref]
-    ema_12 = [Indicators.exp_moving_average(prices, k, 12) for k in range(n_ref//2, n_ref)]
-    ema_26 = [Indicators.exp_moving_average(prices, k, 26) for k in range(n_ref//2, n_ref)]
-    macd = [Indicators.moving_average_conv_div(prices, k) for k in range(n_ref//2, n_ref)]
-    signal = [Indicators.moving_average_conv_div_ema(prices, k) for k in range(n_ref//2, n_ref)]
+    ema_12 = [Indicators.exp_moving_average(
+        prices, k, 12) for k in range(n_ref//2, n_ref)]
+    ema_26 = [Indicators.exp_moving_average(
+        prices, k, 26) for k in range(n_ref//2, n_ref)]
+    macd = [Indicators.moving_average_conv_div(
+        prices, k) for k in range(n_ref//2, n_ref)]
+    signal = [Indicators.moving_average_conv_div_ema(
+        prices, k) for k in range(n_ref//2, n_ref)]
 
     plt.plot(range(0, n_ref//2), ema_12)
     plt.plot(range(0, n_ref//2), ema_26)
@@ -121,8 +132,10 @@ def graph(klines):
 
 def simulate(**kwargs):
 
-    parser = argparse.ArgumentParser(description='Simulation on crypto currency trading strategies')
-    parser.add_argument('-v', '--verbose', help='Display more logs', action='store_true')
+    parser = argparse.ArgumentParser(
+        description='Simulation on crypto currency trading strategies')
+    parser.add_argument('-v', '--verbose',
+                        help='Display more logs', action='store_true')
     args = parser.parse_args()
 
     log_format = '%(asctime)-15s %(message)s'
@@ -135,9 +148,8 @@ def simulate(**kwargs):
     n_ref = 100
     commission = .001
 
-    klines = read_data.read_klines_from_json(file_path='data/binance_klines_XRPUSDT_6h_1525413600000.json')
-
-    # logging.info(klines[36000].close_time)
+    klines = read_data.read_klines_from_json(
+        file_path='data/binance_klines_ETHUSDT_4h_1502942400000.json')
 
     run_simulation(klines, n_ref=n_ref, commission=commission)
 
--- strategy/ema.py
+++ strategy/ema.py
@@ -7,16 +7,23 @@ from model import TradeAction
 
 class EmaStrategy:
 
-    EMA_RANGE = 10
+    EMA_RANGE = 25
     EMA_GAP = 0.
 
     @classmethod
     def decide_action_from_data(cls, klines):
         current_price = klines[-1].close_price
-        typical_prices = [(kline.close_price + kline.high_price + kline.low_price) / 3 for kline in klines]
-        ema = Indicators.exp_moving_average(typical_prices, len(klines) - 1, cls.EMA_RANGE)
-        condition_ema_buy = current_price > (1. + cls.EMA_GAP) * ema
-        condition_ema_sell = current_price < (1. - cls.EMA_GAP) * ema
+        # typical_prices = [(kline.close_price + kline.high_price +
+        #                    kline.low_price) / 3 for kline in klines]
+        close_prices = [kline.close_price for kline in klines]
+        ema = Indicators.exp_moving_average(
+            close_prices, len(klines) - 1, cls.EMA_RANGE)
+        previous_ema = Indicators.exp_moving_average(
+            close_prices, len(klines) - 2, cls.EMA_RANGE)
+        condition_ema_buy = ema > previous_ema and current_price > (
+            1. + cls.EMA_GAP) * ema
+        condition_ema_sell = ema < previous_ema and current_price < (
+            1. - cls.EMA_GAP) * ema
         logging.debug('Ratio to EMA: {}'.format(current_price / ema))
         if condition_ema_buy:
             return TradeAction('buy')

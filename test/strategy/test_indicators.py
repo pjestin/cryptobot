e186ec8cf59c1119303f8839f8dc0b3f1fff86de
Pierre Jestin <pierre.jestin@gmail.com>
2021-01-10 11:19:22 +0000

    Fix tests

--- test/strategy/test_indicators.py
+++ test/strategy/test_indicators.py
@@ -2,30 +2,40 @@ import unittest
 
 from strategy.indicators import Indicators
 
+
 class IndicatorsTest(unittest.TestCase):
 
     def setUp(self):
         self.x = [1., 3., 2.5, 3.4, .4, 4.5, .9, 0.1, .4]
 
     def test_simple_moving_average(self):
-        self.assertEqual(.4, Indicators.simple_moving_average(self.x, K=8, nb_period=1))
-        self.assertEqual(.25, Indicators.simple_moving_average(self.x, K=8, nb_period=2))
-        self.assertEqual(1.475, Indicators.simple_moving_average(self.x, K=8, nb_period=4))
+        self.assertEqual(.4, Indicators.simple_moving_average(
+            self.x, K=8, nb_period=1))
+        self.assertEqual(.25, Indicators.simple_moving_average(
+            self.x, K=8, nb_period=2))
+        self.assertEqual(1.475, Indicators.simple_moving_average(
+            self.x, K=8, nb_period=4))
 
     def test_exp_weights(self):
         self.assertEqual([1.], Indicators.get_exp_weights(nb_period=1))
-        self.assertEqual([0.7499999999999999, 0.25], Indicators.get_exp_weights(nb_period=2))
-        self.assertEqual([0.5714285714285714, 0.2857142857142857, 0.14285714285714285], Indicators.get_exp_weights(nb_period=3))
+        self.assertEqual([0.7499999999999999, 0.25],
+                         Indicators.get_exp_weights(nb_period=2))
+        self.assertEqual([0.5714285714285714, 0.2857142857142857,
+                          0.14285714285714285], Indicators.get_exp_weights(nb_period=3))
 
     def test_exp_moving_average(self):
-        self.assertEqual(.4, Indicators.exp_moving_average(self.x, K=8, nb_period=1))
-        self.assertEqual(.325, Indicators.exp_moving_average(self.x, K=8, nb_period=2))
-        self.assertEqual(.806985294117647, Indicators.exp_moving_average(self.x, K=8, nb_period=4))
-    
+        self.assertEqual(.4, Indicators.exp_moving_average(
+            self.x, K=8, nb_period=1))
+        self.assertEqual(.325, Indicators.exp_moving_average(
+            self.x, K=8, nb_period=2))
+        self.assertEqual(.806985294117647, Indicators.exp_moving_average(
+            self.x, K=8, nb_period=4))
+
     def test_macd(self):
         x = [1., 3., 2.5, 3.4, .4, 4.5, .9, 0.1, .4, 1., 3., 2.5, 3.4, .4, 4.5, .9, 0.1,
-            .4, 1., 3., 2.5, 3.4, .4, 4.5, .9, 0.1, .4, 1., 3., 2.5, 3.4, .4, 4.5, .9, 0.1, .4]
-        self.assertEqual(-0.2538752409938012, Indicators.macd_difference(x, len(x) - 1))
+             .4, 1., 3., 2.5, 3.4, .4, 4.5, .9, 0.1, .4, 1., 3., 2.5, 3.4, .4, 4.5, .9, 0.1, .4]
+        self.assertEqual(-0.253875240993801,
+                         Indicators.macd_difference(x, len(x) - 1))
 
     def test_rsi(self):
         self.assertEqual(0., Indicators.rsi(self.x, 0, 0))
@@ -38,15 +48,19 @@ class IndicatorsTest(unittest.TestCase):
         self.assertEqual(None, Indicators.stoch_rsi(self.x, 0, 0))
         self.assertEqual(None, Indicators.stoch_rsi(self.x, 0, 1))
         self.assertEqual(0., Indicators.stoch_rsi(self.x, 0, 5))
-        self.assertEqual(.20290607161390783, Indicators.stoch_rsi(self.x, 0, 7))
-        self.assertEqual(.39258693609022444, Indicators.stoch_rsi(self.x, 0, 9))
+        self.assertEqual(.20290607161390783,
+                         Indicators.stoch_rsi(self.x, 0, 7))
+        self.assertEqual(.39258693609022444,
+                         Indicators.stoch_rsi(self.x, 0, 9))
 
     def test_mfi(self):
         volumes = [500., 200., 59., 550., 770., 1200., 1150., 100., .4]
         self.assertEqual(0., Indicators.mfi(self.x, volumes, 0, 0))
         self.assertEqual(0., Indicators.mfi(self.x, volumes, 0, 1))
-        self.assertEqual(.4825582531454046, Indicators.mfi(self.x, volumes, 0, 9))
-        self.assertEqual(.4853295513465703, Indicators.mfi(self.x, volumes, 4, 9))
+        self.assertEqual(.4825582531454046,
+                         Indicators.mfi(self.x, volumes, 0, 9))
+        self.assertEqual(.4853295513465703,
+                         Indicators.mfi(self.x, volumes, 4, 9))
 
     def test_log_returns(self):
         expected_returns = [
@@ -63,6 +77,9 @@ class IndicatorsTest(unittest.TestCase):
         self.assertEqual(expected_returns, Indicators.log_returns(self.x))
 
     def test_standard_deviation(self):
-        self.assertEqual(0.0, Indicators.standard_deviation(self.x, K=8, nb_period=1))
-        self.assertEqual(0.15000000000000002, Indicators.standard_deviation(self.x, K=8, nb_period=2))
-        self.assertEqual(1.769710428290459, Indicators.standard_deviation(self.x, K=8, nb_period=4))
+        self.assertEqual(0.0, Indicators.standard_deviation(
+            self.x, K=8, nb_period=1))
+        self.assertEqual(0.15000000000000002, Indicators.standard_deviation(
+            self.x, K=8, nb_period=2))
+        self.assertEqual(1.769710428290459, Indicators.standard_deviation(
+            self.x, K=8, nb_period=4))

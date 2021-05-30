c9685e80b60da4f3f327ee3d14f54c5bda611f3f
Pierre Jestin <pierre.jestin@gmail.com>
2021-05-30 14:04:24 +0200

    Fix rename

--- interface/binance_io.py
+++ interface/binance_io.py
@@ -7,110 +7,118 @@ from binance.client import Client
 from model import Kline, Trade
 
 
-class BinanceInterface():
+class BinanceInterface:
 
     TIMEOUT = 20
-    BINANCE_KEY_FILE = '.binance'
+    BINANCE_KEY_FILE = ".binance"
 
     def __init__(self, client=None):
         if client:
             self.client = client
         else:
-            with open(self.BINANCE_KEY_FILE, newline='\n') as file:
-                api_key = next(file).rstrip('\n')
-                secret_key = next(file).rstrip('\n')
+            with open(self.BINANCE_KEY_FILE, newline="\n") as file:
+                api_key = next(file).rstrip("\n")
+                secret_key = next(file).rstrip("\n")
             self.client = Client(api_key, secret_key)
 
-    def get_history(self, limit, currency_pair):
-        trades = self.client.get_recent_trades(
-            symbol=currency_pair, limit=limit)
-        t = [int(trade['time']) for trade in trades]
-        x = [float(trade['price']) for trade in trades]
+    def get_history(self, limit, symbol):
+        trades = self.client.get_recent_trades(symbol=symbol, limit=limit)
+        t = [int(trade["time"]) for trade in trades]
+        x = [float(trade["price"]) for trade in trades]
         return t, x
 
-    def get_klines(self, limit, interval, currency_pair, start_time=None, end_time=None):
+    def get_klines(self, limit, interval, symbol, start_time=None, end_time=None):
         klines = None
         try:
             klines = self.client.get_klines(
-                symbol=currency_pair,
+                symbol=symbol,
                 interval=interval,
                 limit=limit,
                 startTime=start_time,
-                endTime=end_time
+                endTime=end_time,
             )
         except Exception as e:
-            logging.error('Error retrieving klines: {}'.format(e))
+            logging.error("Error retrieving klines: {}".format(e))
         if not klines:
-            logging.error('Kline data invalid')
+            logging.error("Kline data invalid")
             return None
         return [Kline(kline_data) for kline_data in klines]
 
-    def dump_historical_klines(self, interval, currency_pair, start_time, end_time):
-        klines = list(self.client.get_historical_klines_generator(
-            symbol=currency_pair,
-            interval=interval,
-            start_str=start_time,
-            end_str=end_time
-        ))
-        logging.info('Number of klines: {}'.format(len(klines)))
+    def dump_historical_klines(self, interval, symbol, start_time, end_time):
+        klines = list(
+            self.client.get_historical_klines_generator(
+                symbol=symbol,
+                interval=interval,
+                start_str=start_time,
+                end_str=end_time,
+            )
+        )
+        logging.info("Number of klines: {}".format(len(klines)))
         first_time = klines[0][0]
-        file_name = 'data/binance_klines_{}_{}_{}.json'.format(
-            currency_pair, interval, first_time)
-        with open(file_name, mode='w') as file:
+        file_name = "data/binance_klines_{}_{}_{}.json".format(
+            symbol, interval, first_time
+        )
+        with open(file_name, mode="w") as file:
             json.dump(klines, file)
 
-    def last_price(self, currency_pair):
+    def last_price(self, symbol):
         try:
-            ticker = self.client.get_ticker(symbol=currency_pair)
+            ticker = self.client.get_ticker(symbol=symbol)
         except:
-            logging.error('Error retrieving last price')
-        if not ticker or 'lastPrice' not in ticker:
-            logging.error('Price data invalid')
+            logging.error("Error retrieving last price")
+        if not ticker or "lastPrice" not in ticker:
+            logging.error("Price data invalid")
             return None
-        return float(ticker['lastPrice'])
+        return float(ticker["lastPrice"])
 
-    def create_order(self, is_buy, quantity, currency_pair):
+    def create_order(self, is_buy, quantity, symbol):
         order = self.client.create_order(
-            symbol=currency_pair,
+            symbol=symbol,
             side=Client.SIDE_BUY if is_buy else Client.SIDE_SELL,
             type=Client.ORDER_TYPE_MARKET,
             quantity=quantity,
         )
-        logging.info('Order: {}'.format(order))
-        if not order or 'orderId' not in order:
-            raise ValueError('No order ID in returned order')
-        order_id = order['orderId']
+        logging.info("Order: {}".format(order))
+        if not order or "orderId" not in order:
+            raise ValueError("No order ID in returned order")
+        order_id = order["orderId"]
         for _ in range(0, self.TIMEOUT):
-            if not order or 'status' not in order or order['status'] != Client.ORDER_STATUS_FILLED:
-                logging.info('Awaiting order filling...')
+            if (
+                not order
+                or "status" not in order
+                or order["status"] != Client.ORDER_STATUS_FILLED
+            ):
+                logging.info("Awaiting order filling...")
                 time.sleep(1)
-                order = self.client.get_order(
-                    symbol=currency_pair,
-                    orderId=order_id
-                )
-                logging.info('Waiting on order: {}'.format(order))
+                order = self.client.get_order(symbol=symbol, orderId=order_id)
+                logging.info("Waiting on order: {}".format(order))
         return order
 
     def server_time_diff(self):
         for _ in range(1, 10):
             local_time1 = int(time.time() * 1000)
             server_time = self.client.get_server_time()
-            diff1 = server_time['serverTime'] - local_time1
+            diff1 = server_time["serverTime"] - local_time1
             local_time2 = int(time.time() * 1000)
-            diff2 = local_time2 - server_time['serverTime']
-            print("local1: %s server:%s local2: %s diff1:%s diff2:%s" % (
-                local_time1, server_time['serverTime'], local_time2, diff1, diff2))
+            diff2 = local_time2 - server_time["serverTime"]
+            print(
+                "local1: %s server:%s local2: %s diff1:%s diff2:%s"
+                % (local_time1, server_time["serverTime"], local_time2, diff1, diff2)
+            )
             time.sleep(2)
 
-    def my_trade_history(self, currency_pair):
-        trade_data = self.client.get_my_trades(symbol=currency_pair)
+    def my_trade_history(self, symbol):
+        trade_data = self.client.get_my_trades(symbol=symbol)
         trades = [Trade(trade) for trade in trade_data]
         merged_trades = []
         previous_trade_time = None
         previous_trade_is_buy = None
         for trade in trades:
-            if previous_trade_time and abs(trade.time - previous_trade_time) < 60. \
-                    and trade.is_buy == previous_trade_is_buy:
+            if (
+                previous_trade_time
+                and abs(trade.time - previous_trade_time) < 60.0
+                and trade.is_buy == previous_trade_is_buy
+            ):
                 merged_trades[-1].quantity += trade.quantity
             else:
                 merged_trades.append(trade)
@@ -118,7 +126,7 @@ class BinanceInterface():
             previous_trade_is_buy = trade.is_buy
         return merged_trades
 
-    def last_trade(self, currency_pair):
-        if not self.my_trade_history(currency_pair):
+    def last_trade(self, symbol):
+        if not self.my_trade_history(symbol):
             return None
-        return self.my_trade_history(currency_pair)[-1]
+        return self.my_trade_history(symbol)[-1]
--- test/interface/test_binance_io.py
+++ test/interface/test_binance_io.py
@@ -11,7 +11,7 @@ from interface import read_data
 
 class BinanceInterfaceTest(unittest.TestCase):
 
-    TEST_KLINE_DATA_FILE = 'data/binance_klines_DOGEUSDT_2m_1509926400000.json'
+    TEST_KLINE_DATA_FILE = "data/binance_klines_DOGEUSDT_2m_1509926400000.json"
 
     def setUp(self):
         if os.path.isfile(self.TEST_KLINE_DATA_FILE):
@@ -22,31 +22,31 @@ class BinanceInterfaceTest(unittest.TestCase):
 
         self.kline1 = [
             1509926400000,
-            '1.50000000',
-            '1.79900000',
-            '0.50000000',
-            '1.54580000',
-            '15425.04000000',
+            "1.50000000",
+            "1.79900000",
+            "0.50000000",
+            "1.54580000",
+            "15425.04000000",
             1509947999999,
-            '23698.98992800',
+            "23698.98992800",
             199,
-            '2901.96000000',
-            '4663.35341300',
-            '102181628.99137327'
+            "2901.96000000",
+            "4663.35341300",
+            "102181628.99137327",
         ]
         self.kline2 = [
             1509948000000,
-            '1.54580000',
-            '1.68100000',
-            '1.53870000',
-            '1.62880000',
-            '59449.05000000',
+            "1.54580000",
+            "1.68100000",
+            "1.53870000",
+            "1.62880000",
+            "59449.05000000",
             1509969599999,
-            '95921.08691000',
+            "95921.08691000",
             338,
-            '26110.78000000',
-            '42788.24647100',
-            '102286567.18669863'
+            "26110.78000000",
+            "42788.24647100",
+            "102286567.18669863",
         ]
 
     def tearDown(self):
@@ -56,20 +56,19 @@ class BinanceInterfaceTest(unittest.TestCase):
     def test_recent_trades(self):
         self.mock_client.get_recent_trades.return_value = [
             {
-                'time': 1564521434,
-                'price': 105.23,
+                "time": 1564521434,
+                "price": 105.23,
             },
             {
-                'time': 1564555002,
-                'price': 104.22,
+                "time": 1564555002,
+                "price": 104.22,
             },
         ]
-        t, x = self.binance.get_history(2, 'BTCUSDT')
+        t, x = self.binance.get_history(2, "BTCUSDT")
         self.assertEqual([1564521434, 1564555002], t)
         self.assertEqual([105.23, 104.22], x)
         self.mock_client.get_recent_trades.assert_called_once_with(
-            symbol='BTCUSDT',
-            limit=2
+            symbol="BTCUSDT", limit=2
         )
 
     def test_klines(self):
@@ -77,108 +76,83 @@ class BinanceInterfaceTest(unittest.TestCase):
         kline_objects = [Kline(self.kline1), Kline(self.kline2)]
         klines = self.binance.get_klines(
             limit=2,
-            interval='5m',
-            currency_pair='BNBBTC',
-            start_time='2 years ago',
-            end_time='now'
+            interval="5m",
+            symbol="BNBBTC",
+            start_time="2 years ago",
+            end_time="now",
         )
         self.assertEqual(kline_objects, klines)
         self.mock_client.get_klines.assert_called_once_with(
-            symbol='BNBBTC',
-            interval='5m',
+            symbol="BNBBTC",
+            interval="5m",
             limit=2,
-            startTime='2 years ago',
-            endTime='now'
+            startTime="2 years ago",
+            endTime="now",
         )
 
     def test_klines_error(self):
-        self.mock_client.get_klines.side_effect = ValueError('Not found')
+        self.mock_client.get_klines.side_effect = ValueError("Not found")
         self.binance.get_klines(
             limit=2,
-            interval='5m',
-            currency_pair='BNBBTC',
-            start_time='2 years ago',
-            end_time='now'
+            interval="5m",
+            symbol="BNBBTC",
+            start_time="2 years ago",
+            end_time="now",
         )
 
     def test_dump_historical_klines(self):
         self.mock_client.get_historical_klines_generator.return_value = [
-            self.kline1, self.kline2]
+            self.kline1,
+            self.kline2,
+        ]
         self.binance.dump_historical_klines(
-            interval='2m',
-            currency_pair='DOGEUSDT',
-            start_time='2 years ago',
-            end_time='now'
+            interval="2m",
+            symbol="DOGEUSDT",
+            start_time="2 years ago",
+            end_time="now",
         )
         kline_data = read_data.read_klines_from_json(self.TEST_KLINE_DATA_FILE)
         self.assertEqual([Kline(self.kline1), Kline(self.kline2)], kline_data)
 
     def test_last_price(self):
-        self.mock_client.get_ticker.return_value = {'lastPrice': 95.62}
-        last_price = self.binance.last_price('BTCUSDT')
+        self.mock_client.get_ticker.return_value = {"lastPrice": 95.62}
+        last_price = self.binance.last_price("BTCUSDT")
         self.assertEqual(95.62, last_price)
-        self.mock_client.get_ticker.assert_called_once_with(symbol='BTCUSDT')
+        self.mock_client.get_ticker.assert_called_once_with(symbol="BTCUSDT")
 
     def test_create_order_immediate(self):
-        mock_order = {
-            'orderId': 153118,
-            'status': 'FILLED'
-        }
+        mock_order = {"orderId": 153118, "status": "FILLED"}
         self.mock_client.create_order.return_value = mock_order
-        order = self.binance.create_order(
-            is_buy=True,
-            quantity=2.5,
-            currency_pair='BTCUSDT'
-        )
+        order = self.binance.create_order(is_buy=True, quantity=2.5, symbol="BTCUSDT")
         self.assertEqual(mock_order, order)
         self.mock_client.create_order.assert_called_once_with(
-            symbol='BTCUSDT',
-            side='BUY',
-            type='MARKET',
+            symbol="BTCUSDT",
+            side="BUY",
+            type="MARKET",
             quantity=2.5,
         )
 
     def test_create_order_delay(self):
-        mock_order_pending = {
-            'orderId': 153118,
-            'status': 'PENDING'
-        }
-        mock_order_filled = {
-            'orderId': 153118,
-            'status': 'FILLED'
-        }
+        mock_order_pending = {"orderId": 153118, "status": "PENDING"}
+        mock_order_filled = {"orderId": 153118, "status": "FILLED"}
         self.mock_client.create_order.return_value = mock_order_pending
-        self.mock_client.get_order.side_effect = [
-            mock_order_pending, mock_order_filled]
-        order = self.binance.create_order(
-            is_buy=False,
-            quantity=2.5,
-            currency_pair='BTCUSDT'
-        )
+        self.mock_client.get_order.side_effect = [mock_order_pending, mock_order_filled]
+        order = self.binance.create_order(is_buy=False, quantity=2.5, symbol="BTCUSDT")
         self.assertEqual(mock_order_filled, order)
         self.mock_client.create_order.assert_called_with(
-            symbol='BTCUSDT',
-            side='SELL',
-            type='MARKET',
+            symbol="BTCUSDT",
+            side="SELL",
+            type="MARKET",
             quantity=2.5,
         )
-        self.mock_client.get_order.assert_called_with(
-            symbol='BTCUSDT',
-            orderId=153118
-        )
+        self.mock_client.get_order.assert_called_with(symbol="BTCUSDT", orderId=153118)
 
     def test_create_order_failure(self):
-        mock_order_failure = {
-            'status': 'FAIL'
-        }
+        mock_order_failure = {"status": "FAIL"}
         self.mock_client.create_order.return_value = mock_order_failure
         with self.assertRaises(ValueError) as cm:
-            self.binance.create_order(
-                is_buy=True,
-                quantity=2.5,
-                currency_pair='BTCUSDT'
-            )
-        self.assertEqual('No order ID in returned order', str(cm.exception))
+            self.binance.create_order(is_buy=True, quantity=2.5, symbol="BTCUSDT")
+        self.assertEqual("No order ID in returned order", str(cm.exception))
 
     def test_my_trade_history(self):
         mock_trade1 = {
@@ -217,26 +191,35 @@ class BinanceInterfaceTest(unittest.TestCase):
             "isBuyer": False,
         }
 
-        self.mock_client.get_my_trades.return_value = [mock_trade1, mock_trade2,
-            mock_trade3, mock_trade4, mock_trade5]
-        trades = self.binance.my_trade_history(currency_pair='BTCUSDT')
-        self.mock_client.get_my_trades.assert_called_with(symbol='BTCUSDT')
+        self.mock_client.get_my_trades.return_value = [
+            mock_trade1,
+            mock_trade2,
+            mock_trade3,
+            mock_trade4,
+            mock_trade5,
+        ]
+        trades = self.binance.my_trade_history(symbol="BTCUSDT")
+        self.mock_client.get_my_trades.assert_called_with(symbol="BTCUSDT")
         self.assertEqual(3, len(trades))
 
-        self.assertEqual(float(mock_trade1['price']), trades[0].price)
+        self.assertEqual(float(mock_trade1["price"]), trades[0].price)
         self.assertTrue(trades[0].is_buy)
-        self.assertEqual(mock_trade1['time'] / 1000., trades[0].time)
-        self.assertEqual(float(mock_trade1['qty']) + float(mock_trade2['qty']), trades[0].quantity)
+        self.assertEqual(mock_trade1["time"] / 1000.0, trades[0].time)
+        self.assertEqual(
+            float(mock_trade1["qty"]) + float(mock_trade2["qty"]), trades[0].quantity
+        )
 
-        self.assertEqual(float(mock_trade3['price']), trades[1].price)
+        self.assertEqual(float(mock_trade3["price"]), trades[1].price)
         self.assertFalse(trades[1].is_buy)
-        self.assertEqual(mock_trade3['time'] / 1000., trades[1].time)
-        self.assertEqual(float(mock_trade3['qty']), trades[1].quantity)
+        self.assertEqual(mock_trade3["time"] / 1000.0, trades[1].time)
+        self.assertEqual(float(mock_trade3["qty"]), trades[1].quantity)
 
-        self.assertEqual(float(mock_trade4['price']), trades[2].price)
+        self.assertEqual(float(mock_trade4["price"]), trades[2].price)
         self.assertFalse(trades[2].is_buy)
-        self.assertEqual(mock_trade4['time'] / 1000., trades[2].time)
-        self.assertEqual(float(mock_trade4['qty']) + float(mock_trade5['qty']), trades[2].quantity)
+        self.assertEqual(mock_trade4["time"] / 1000.0, trades[2].time)
+        self.assertEqual(
+            float(mock_trade4["qty"]) + float(mock_trade5["qty"]), trades[2].quantity
+        )
 
     def test_last_trade(self):
         mock_trade = {
@@ -248,11 +231,11 @@ class BinanceInterfaceTest(unittest.TestCase):
             "time": 1499865549590,
             "isBuyer": True,
             "isMaker": False,
-            "isBestMatch": True
+            "isBestMatch": True,
         }
         self.mock_client.get_my_trades.return_value = [mock_trade]
-        last_trade = self.binance.last_trade(currency_pair='BTCUSDT')
-        self.mock_client.get_my_trades.assert_called_with(symbol='BTCUSDT')
-        self.assertEqual(float(mock_trade['price']), last_trade.price)
-        self.assertEqual(mock_trade['isBuyer'], last_trade.is_buy)
-        self.assertEqual(mock_trade['time'] / 1000., last_trade.time)
+        last_trade = self.binance.last_trade(symbol="BTCUSDT")
+        self.mock_client.get_my_trades.assert_called_with(symbol="BTCUSDT")
+        self.assertEqual(float(mock_trade["price"]), last_trade.price)
+        self.assertEqual(mock_trade["isBuyer"], last_trade.is_buy)
+        self.assertEqual(mock_trade["time"] / 1000.0, last_trade.time)
